bd69b944ed1579d469cd9858ea328fcdcd80efeb
Ben Salinas <ben.salinas@gmail.com>
2014-11-05 12:07:22 -0500

    Initial revision of svg-crowbar

--- svg-crowbar-2.js
+++ svg-crowbar-2.js
@@ -1,4 +1,4 @@
-(function() {
+var crowbar = (function() {
   var doctype = '<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">';
 
   window.URL = (window.URL || window.webkitURL);
@@ -12,7 +12,6 @@
     svg: "http://www.w3.org/2000/svg"
   };
 
-  initialize();
 
   function initialize() {
     var documents = [window.document],
@@ -52,95 +51,9 @@
         SVGSources.push(newSources[i]);
       }
     });
-    if (SVGSources.length > 1) {
-      createPopover(SVGSources);
-    } else if (SVGSources.length > 0) {
-      download(SVGSources[0]);
-    } else {
-      alert("The Crowbar couldn’t find any SVG nodes.");
-    }
+    return SVGSources;
 
   }
-
-  function createPopover(sources) {
-    cleanup();
-
-    sources.forEach(function(s1) {
-      sources.forEach(function(s2) {
-        if (s1 !== s2) {
-          if ((Math.abs(s1.top - s2.top) < 38) && (Math.abs(s1.left - s2.left) < 38)) {
-            s2.top += 38;
-            s2.left += 38;
-          }
-        }
-      });
-    });
-
-    var buttonsContainer = document.createElement("div");
-    body.appendChild(buttonsContainer);
-
-    buttonsContainer.setAttribute("class", "svg-crowbar");
-    buttonsContainer.style["z-index"] = 1e7;
-    buttonsContainer.style["position"] = "absolute";
-    buttonsContainer.style["top"] = 0;
-    buttonsContainer.style["left"] = 0;
-
-
-
-    var background = document.createElement("div");
-    body.appendChild(background);
-
-    background.setAttribute("class", "svg-crowbar");
-    background.style["background"] = "rgba(255, 255, 255, 0.7)";
-    background.style["position"] = "fixed";
-    background.style["left"] = 0;
-    background.style["top"] = 0;
-    background.style["width"] = "100%";
-    background.style["height"] = "100%";
-
-    sources.forEach(function(d, i) {
-      var buttonWrapper = document.createElement("div");
-      buttonsContainer.appendChild(buttonWrapper);
-      buttonWrapper.setAttribute("class", "svg-crowbar");
-      buttonWrapper.style["position"] = "absolute";
-      buttonWrapper.style["top"] = (d.top + document.body.scrollTop) + "px";
-      buttonWrapper.style["left"] = (document.body.scrollLeft + d.left) + "px";
-      buttonWrapper.style["padding"] = "4px";
-      buttonWrapper.style["border-radius"] = "3px";
-      buttonWrapper.style["color"] = "white";
-      buttonWrapper.style["text-align"] = "center";
-      buttonWrapper.style["font-family"] = "'Helvetica Neue'";
-      buttonWrapper.style["background"] = "rgba(0, 0, 0, 0.8)";
-      buttonWrapper.style["box-shadow"] = "0px 4px 18px rgba(0, 0, 0, 0.4)";
-      buttonWrapper.style["cursor"] = "move";
-      buttonWrapper.textContent =  "SVG #" + i + ": " + (d.id ? "#" + d.id : "") + (d.class ? "." + d.class : "");
-
-      var button = document.createElement("button");
-      buttonWrapper.appendChild(button);
-      button.setAttribute("data-source-id", i);
-      button.style["width"] = "150px";
-      button.style["font-size"] = "12px";
-      button.style["line-height"] = "1.4em";
-      button.style["margin"] = "5px 0 0 0";
-      button.textContent = "Download";
-
-      button.onclick = function(el) {
-        // console.log(el, d, i, sources)
-        download(d);
-      };
-
-    });
-
-  }
-
-  function cleanup() {
-    var crowbarElements = document.querySelectorAll(".svg-crowbar");
-
-    [].forEach.call(crowbarElements, function(el) {
-      el.parentNode.removeChild(el);
-    });
-  }
-
 
   function getSources(doc, emptySvgDeclarationComputed) {
     var svgInfo = [],
@@ -181,6 +94,9 @@
     return svgInfo;
   }
 
+  function prepareBlob(source){
+    return new Blob(source.source, { "type" : "text\/xml" });
+  }
   function download(source) {
     var filename = "untitled";
 
@@ -192,7 +108,8 @@
       filename = window.document.title.replace(/[^a-z0-9]/gi, '-').toLowerCase();
     }
 
-    var url = window.URL.createObjectURL(new Blob(source.source, { "type" : "text\/xml" }));
+    var url = window.URL.createObjectURL(prepareBlob(source));
+    
 
     var a = document.createElement("a");
     body.appendChild(a);
@@ -255,5 +172,9 @@
     }
   }
 
-
+  return{
+    initialize:initialize,
+    download:download,
+    prepareBlob:prepareBlob
+  }
 })();
